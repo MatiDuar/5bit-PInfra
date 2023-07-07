3a499bf47992a4cd02714ee0a1627f2cbb168d06
MatiDuar <matias.duarte@estudiantes.utec.edu.uy>
2023-07-07 00:31:17 -0300

    comentario con validador del token y validador de expiracion

--- src/main/java/com/beans/LoginBeanJWT.java
+++ src/main/java/com/beans/LoginBeanJWT.java
@@ -1,8 +1,10 @@
 package com.beans;
 
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.impl.DefaultClaims;
 import io.jsonwebtoken.security.Keys;
 
 
@@ -41,12 +43,15 @@ public class LoginBeanJWT implements Serializable {
 
 	    public void generarToken() {
 	        // a esta altura ya se valido el usuario, solo hay que generar el Token
-
-	            // Generar el token JWT
-	            String secretKey = "clave_secreta"; // Clave secreta para firmar el token, estas clave deberia ser mas robusta y deberia estar mas oculta.
+	    	
+	    	// Generar el token JWT
+            String secretKey = "clave_secreta"; // Clave secreta para firmar el token, estas clave deberia ser mas robusta y deberia estar mas oculta.
+            String token = null;
+	    	try {
+	    		
 	            Date expirationDate = new Date(System.currentTimeMillis() + 86400000); // 1 día de expiración
 
-	            String token = Jwts.builder()
+	            token = Jwts.builder()
 	                    .setSubject(nombreUsuario)
 	                    .setExpiration(expirationDate)
 	                    .signWith(Keys.hmacShaKeyFor(secretKey.getBytes()), SignatureAlgorithm.HS256)
@@ -54,6 +59,44 @@ public class LoginBeanJWT implements Serializable {
 
 	            // Almacenar el token en la sesión
 	            FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("token", token);
+	            
+	    	}catch (Exception e){
+	    		// El token no se genero
+	    	}
+	            
+	    	
+	    	
+	    	// ****************************************  Esto capaz no lo usamos  **************************************** 
+	    	
+	    	/**
+	    	 
+	    	  	 //esto se usa para saber si el token es valido
+	    	Claims claims = new DefaultClaims();
+	    	try {
+	    	       claims = Jwts.parserBuilder()
+	    	            .setSigningKey(Keys.hmacShaKeyFor(secretKey.getBytes()))
+	    	            .build()
+	    	            .parseClaimsJws(token)
+	    	            .getBody();
+
+	    	    // El token es válido
+	    	} catch (Exception e) {
+	    	    // La firma del token no es válida
+	    	}
+	    	
+	    	// esto es para saber si el token expiro
+	    	Date expirationDate = claims.getExpiration();
+	    	Date currentDate = new Date();
+
+	    	if (expirationDate.before(currentDate)) {
+	    	    // El token ha expirado
+	    	} else {
+	    	    // El token está dentro del período de validez
+	    	} 
+	    	  	
+	    	 */
+	    	
+	    	// ****************************************  Esto capaz no lo usamos  ****************************************
             
 	    }
 
